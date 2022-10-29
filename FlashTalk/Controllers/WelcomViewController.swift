3da96e92a8dc5ee8f0d3500e841fea6a5236c082
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-30 00:35:02 +0900

    modification in a Source Code

--- FlashTalk/Controllers/WelcomViewController.swift
+++ FlashTalk/Controllers/WelcomViewController.swift
@@ -6,13 +6,27 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.text = "⚡️FlashTalk"
+        
+//        titleLabel.text = ""
+//        var charactIndex = 0.0
+//        let titleText = "⚡️FlashTalk"
+//
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charactIndex, repeats: false ) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charactIndex += 1
+//        }
 
        
     }
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '13.0'
+
+target 'FlashTalk' do
+  use_frameworks!
+
+  # Pods for FlashTalk
+  
+  pod 'CLTypingLabel', '~> 0.4.0'
+  
+end
