c3a3e2ba5434e3b6cf94fbdd46f9bf40c99d1d5c
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-30 02:42:57 +0900

    Update RegisterViewController.swift

--- FlashTalk/Controllers/RegisterViewController.swift
+++ FlashTalk/Controllers/RegisterViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -13,8 +14,10 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
-        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
         
+            }
         }
     }
     
