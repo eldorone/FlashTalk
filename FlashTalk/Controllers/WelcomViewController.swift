2afb5835c7578b1e93474cd8340d9f585fad53ef
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-30 00:35:32 +0900

    Update WelcomViewController.swift

--- FlashTalk/Controllers/WelcomViewController.swift
+++ FlashTalk/Controllers/WelcomViewController.swift
@@ -6,11 +6,10 @@
 //
 
 import UIKit
-import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: CLTypingLabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
