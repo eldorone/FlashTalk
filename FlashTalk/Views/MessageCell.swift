188d1c80deec019dbd523a9084b36858833dff5d
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-31 01:04:58 +0900

    modification in the source code

--- FlashTalk/Views/MessageCell.swift
+++ FlashTalk/Views/MessageCell.swift
@@ -10,6 +10,7 @@ import UIKit
 class MessageCell: UITableViewCell {
 
     @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
     
     
     override func awakeFromNib() {
