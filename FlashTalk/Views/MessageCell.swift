8a6ecfd7437cc6fbd6a3a532d6bc9bd05f8086c6
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-31 00:55:19 +0900

    Modification in the Source Code

---
+++ FlashTalk/Views/MessageCell.swift
@@ -0,0 +1,23 @@
+//
+//  MessageCell.swift
+//  FlashTalk
+//
+//  Created by Eldor Alikuvatov on 2022/10/31.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
