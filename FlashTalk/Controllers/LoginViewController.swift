168d7c7d9ea54c162329c6bed214f360578026ee
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-01 02:31:19 +0900

    Update LoginViewController.swift

--- FlashTalk/Controllers/LoginViewController.swift
+++ FlashTalk/Controllers/LoginViewController.swift
@@ -12,7 +12,6 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
-    
 
     @IBAction func loginPressed(_ sender: UIButton) {
         if let email = emailTextfield.text, let password = passwordTextfield.text {
