c54a26e90ec0f7bc262f12871da1660171815f34
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-03 02:51:38 +0900

    Update ChatViewController.swift

--- FlashTalk/Controllers/ChatViewController.swift
+++ FlashTalk/Controllers/ChatViewController.swift
@@ -33,7 +33,7 @@ class ChatViewController: UIViewController {
     
     func loadMessages() {
         
-        dataBase.collection(K.FStore.collectionName).addSnapshotListener { (querySnapshot, error) in
+        dataBase.collection(K.FStore.collectionName).order(by: K.FStore.dateField).addSnapshotListener { (querySnapshot, error) in
             
             self.messages = []
             
@@ -60,7 +60,7 @@ class ChatViewController: UIViewController {
     @IBAction func sendPressed(_ sender: UIButton) {
         
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
-            dataBase.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody])
+            dataBase.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody, K.FStore.dateField: Date().timeIntervalSince1970])
             { (error) in
                 if let e = error {
                     print("There was an issue saving data to Firestore, \(e)")
