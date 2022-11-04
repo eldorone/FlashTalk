7f1ec8288428a0d0540519d23e4ce3e0d9c0cf29
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-04 23:24:26 +0900

    Update ChatViewController.swift

--- FlashTalk/Controllers/ChatViewController.swift
+++ FlashTalk/Controllers/ChatViewController.swift
@@ -90,7 +90,6 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let message = messages[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
         cell.label.text = messages[indexPath.row].body
